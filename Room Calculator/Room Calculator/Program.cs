be55bc1fb1b5632ceb4540c70661550116480dfa
sadams31 <sadams@wwiflint.com>
2022-04-11 14:32:34 -0400

    Initial Commit for Lab

---
+++ Room Calculator/Room Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿
+        var Continue_Running = false;
+do
+{
+    Console.WriteLine("What room would you like to measure?");
+    string room = Console.ReadLine();
+
+    Console.WriteLine("Please enter the length of the " + room + " in feet.");
+
+    decimal length = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Please enter the width of the " +room +  " in feet.");
+
+    decimal width = decimal.Parse(Console.ReadLine());
+
+    decimal area = length * width;
+    decimal perimeter = 2 * (length + width);
+    decimal volume = length * length * length;
+   
+
+    Console.WriteLine(" The area for " + room + " is " + area + " feet");
+    Console.WriteLine(" The perimeter for " + room + " is " + perimeter + " feet");
+
+
+    Console.WriteLine();
+        Console.WriteLine(" Would you like to measure another room?");
+        Console.WriteLine("Please enter yes or no");
+
+        var input = (Console.ReadLine());
+        if (input.ToLower() == "yes" || input.ToLower() == "y")
+        {
+            Continue_Running = true;
+        }
+        else
+        {
+            Continue_Running = false;
+        }
+    
+}
+
+while (Continue_Running == true);
+
+        
+       
